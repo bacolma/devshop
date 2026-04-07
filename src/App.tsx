5399bed475b2962a5524046c1df9a68e3f80ac99
Marcos Colchado <bacolma@gmail.com>
2026-04-06 19:23:28 -0500

    correccion App

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export default function App() {
             : <Navigate to="/login" replace />
         }
       />
-
+ 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
